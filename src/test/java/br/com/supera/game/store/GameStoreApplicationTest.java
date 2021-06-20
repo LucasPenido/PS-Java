bf16f35e48f575dd8b96a821b0f8346d0821740f
Lucas Penido <lpenidoa@me.com>
2021-06-20 13:21:33 -0300

    Adicionando configuraçoes do spring e do H2.

---
+++ src/test/java/br/com/supera/game/store/GameStoreApplicationTest.java
@@ -0,0 +1,11 @@
+package br.com.supera.game.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class GameStoreApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+}
