63519a287f4c81dd8112d975e1476c199f113236
Lucas Penido <lpenidoa@me.com>
2021-06-21 15:30:09 -0300

    Criando endpoint que possibilita a criacao de um carrinho de compras.

--- src/main/java/br/com/supera/game/store/controller/CartController.java
+++ src/main/java/br/com/supera/game/store/controller/CartController.java
@@ -1,13 +1,13 @@
 package br.com.supera.game.store.controller;
 
 import br.com.supera.game.store.dto.CartDTO;
+import br.com.supera.game.store.dto.UserDTO;
+import br.com.supera.game.store.exception.UserNotFoundException;
 import br.com.supera.game.store.service.CartService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,5 +25,11 @@ public class CartController {
     @GetMapping("/{cpf}")
     public CartDTO findCartByUserCpf(@PathVariable String cpf) {
         return cartService.findCartByUserCpf(cpf);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public CartDTO createCart(@RequestBody UserDTO userDTO) throws UserNotFoundException {
+        return cartService.createCart(userDTO);
     }
 }
--- src/main/java/br/com/supera/game/store/service/CartService.java
+++ src/main/java/br/com/supera/game/store/service/CartService.java
@@ -1,13 +1,16 @@
 package br.com.supera.game.store.service;
 
 import br.com.supera.game.store.dto.CartDTO;
+import br.com.supera.game.store.dto.UserDTO;
 import br.com.supera.game.store.entity.Cart;
+import br.com.supera.game.store.exception.UserNotFoundException;
 import br.com.supera.game.store.mapper.CartMapper;
 import br.com.supera.game.store.repository.CartRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -16,6 +19,7 @@ import java.util.stream.Collectors;
 public class CartService {
     private final CartRepository cartRepository;
     private final CartMapper cartMapper = CartMapper.INSTANCE;
+    private final UserService userService;
 
     public List<CartDTO> findAll() {
         return cartRepository.findAll()
@@ -28,5 +32,17 @@ public class CartService {
         Cart cart = cartRepository.findByUserCpf(cpf)
                 .orElseThrow();
         return cartMapper.toDTO(cart);
+    }
+
+    public CartDTO createCart(UserDTO userDTO) throws UserNotFoundException {
+        System.out.println(userDTO);
+        userService.verifyIfUserExists(userDTO.getCpf());
+        CartDTO cartDTO = new CartDTO();
+
+        cartDTO.setUser(userDTO);
+        cartDTO.setDateCreated(new Date());
+        Cart cart = cartMapper.toModel(cartDTO);
+        Cart savedCart = cartRepository.save(cart);
+        return cartMapper.toDTO(savedCart);
     }
 }
